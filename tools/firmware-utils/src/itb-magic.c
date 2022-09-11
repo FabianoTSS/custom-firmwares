301f93b9480f2b2656c22fd1b7fe1badea277554
Fabiano Tassotti <fabianotassotti@gmail.com>
2022-09-11 06:30:24 -0700

    Creating a Tool to itb-magic (intelbras)

---
+++ tools/firmware-utils/src/itb-magic.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2022 Vasilis Tsiligiannis <fabianotassotti@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ */
+
+int main() {
+   // printf() displays the string inside quotation
+   
+   printf("Hello, World!");
+   return 0;
+}
